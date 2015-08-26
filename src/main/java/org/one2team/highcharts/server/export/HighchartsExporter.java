b8440de7986df668af949463a35306f338d44a50
Ernesto Reinaldo <reiern70@gmail.com>
2015-08-26 11:59:19 +0200

    add a method to export to an output stream

--- src/main/java/org/one2team/highcharts/server/export/HighchartsExporter.java
+++ src/main/java/org/one2team/highcharts/server/export/HighchartsExporter.java
@@ -21,29 +21,39 @@ public class HighchartsExporter<T> {
 			                T globalOptions,
 			                File file) {
 		
-		OutputStream fos = null;
 		try {
-			fos = render (chartOptions, globalOptions, file);
-
-		} catch (Exception e) {
+            OutputStream fos = new FileOutputStream (file);
+            export(chartOptions, globalOptions, fos);
+		} catch (FileNotFoundException e) {
 			e.printStackTrace ();
 			throw (new RuntimeException (e));
-		} finally {
-			if (fos != null)
-				IOUtils.closeQuietly (fos);
 		}
 	}
 
-	private OutputStream render (T chartOptions,
-			                         T globalOptions,
-                               File file) throws FileNotFoundException {
-		FileOutputStream fos;
-		renderer.setChartOptions (chartOptions)
-				    .setGlobalOptions (globalOptions)
-				    .setOutputStream (fos = new FileOutputStream (file))
-				    .render ();
-		return fos;
-	}
+    public void export (T chartOptions,
+                        T globalOptions,
+                        OutputStream file) {
+        try {
+            render (chartOptions, globalOptions, file);
+        } catch (Exception e) {
+            e.printStackTrace ();
+            throw (new RuntimeException (e));
+        } finally {
+            if (file != null) {
+                IOUtils.closeQuietly(file);
+            }
+        }
+    }
+
+    private void render (T chartOptions,
+                                 T globalOptions,
+                                 OutputStream out)  {
+        renderer.setChartOptions (chartOptions)
+                .setGlobalOptions (globalOptions)
+                .setOutputStream ( out )
+                .render ();
+    }
+
 
 	public SVGStreamRenderer<T> getRenderer () {
 		return renderer;
